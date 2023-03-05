daa3af927c768c2c9f6dfdeacc76112f9a5bcd3a
GareArc <garethcxy.chen@mail.utoronto.ca>
2023-03-05 15:03:33 -0500

    fix bootstrap and test sentry

--- client/App.js
+++ client/App.js
@@ -1,28 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import * as Sentry from '@sentry/react-native';
+import { Navigation } from './Navigation';
 
 Sentry.init({
   dsn: "https://71ed77cdaeff44e7b814cd90fce00f97@o358880.ingest.sentry.io/4504487922565120",
 });
 
-export default function App() {
+export default App = () => {
   return (
     <Sentry.ErrorBoundary showDialog>
-      <View style={styles.container}>
-        <Text>StylEase by No Brainer Team</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Navigation />
     </Sentry.ErrorBoundary>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ client/Navigation.jsx
@@ -0,0 +1,16 @@
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {Home} from './views/Home';
+
+const Stack = createNativeStackNavigator();
+
+export const Navigation = () => { 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{title: 'Welcome!'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
