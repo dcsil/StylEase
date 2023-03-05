763b5dfc673d67bec360e8542b645261909eb09b
GareArc <garethcxy.chen@mail.utoronto.ca>
2023-03-05 02:10:12 -0500

    fix sentry bug

--- client/App.js
+++ client/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Sentry from '@sentry/react-native';
-import { SentryErrorBoundary } from 'react-native-sentry';
 
 Sentry.init({
   dsn: process.env.DSN,
@@ -10,12 +9,12 @@ Sentry.init({
 
 export default function App() {
   return (
-    <SentryErrorBoundary showDialog>
+    <Sentry.ErrorBoundary showDialog>
       <View style={styles.container}>
         <Text>StylEase by No Brainer Team</Text>
         <StatusBar style="auto" />
       </View>
-    </SentryErrorBoundary>
+    </Sentry.ErrorBoundary>
   );
 }
 
