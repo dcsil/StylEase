38c16824da446d562279493f5b003c5b5a02c790
GareArc <garethcxy.chen@mail.utoronto.ca>
2023-03-05 01:21:15 -0500

    configure client

--- client/App.js
+++ client/App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Sentry from '@sentry/react-native';
+import { SentryErrorBoundary } from 'react-native-sentry';
+
+Sentry.init({
+  dsn: process.env.DSN,
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SentryErrorBoundary showDialog>
+      <View style={styles.container}>
+        <Text>StylEase by No Brainer Team</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SentryErrorBoundary>
   );
 }
 
