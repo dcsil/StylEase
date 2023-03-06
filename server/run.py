403b47b3a5389d893a6b58f16b9b2f34de4b601e
ZhouEEEEEE <zhoueric0603@gmail.com>
2023-03-05 21:42:00 -0500

    Flask connected to the Database (MongoDB)

--- server/run.py
+++ server/run.py
@@ -1,6 +1,10 @@
 from app import app
 import os
+from dotenv import load_dotenv
+
 
 if __name__ == "__main__":
+    load_dotenv()
+    # print(os.environ.get('test'))
     # print(os.environ.get("MONGODB_URL"))
     app.run(debug=True, port=int(os.environ.get('PORT', 7000)))
