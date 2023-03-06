3760dd1c3fae2e61444c6ffb9b118bf92256381a
ZhouEEEEEE <zhoueric0603@gmail.com>
2023-03-06 00:11:15 -0500

    Fix Error R10

--- server/run.py
+++ server/run.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     load_dotenv()
     # print(os.environ.get('test'))
     # print(os.environ.get("MONGODB_URL"))
-    app.run(debug=True, port=int(os.environ.get('PORT', 7000)))
+    app.run(host='0.0.0.0', debug=True, port=int(os.environ.get('PORT', 7000)))
