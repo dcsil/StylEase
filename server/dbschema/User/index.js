122c825452b210fbe3aece19a4452a29f16861a7
ZhouEEEEEE <zhoueric0603@gmail.com>
2023-03-06 17:08:28 -0500

    Add user database schema

---
+++ server/dbschema/User/index.js
@@ -0,0 +1,38 @@
+const { mongoose } = require('../../mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: false
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    calendar: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    wardrobe: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    outfits: {
+        type: [mongoose.Schema.Types.ObjectId],
+    },
+    friends: {
+        type: [mongoose.Schema.Types.ObjectId],
+    },
+    subscription: {
+        type: [mongoose.Schema.Types.ObjectId],
+    },
+    preference_tags: {
+        type: mongoose.Schema.Types.Mixed,
+    }
+});
+
+module.exports = {
+    User: mongoose.model('User', UserSchema)
+}
