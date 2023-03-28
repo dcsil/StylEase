732ac0978ed5adcccae183de65451aeb8662457b
ZhouEEEEEE <zhoueric0603@gmail.com>
2023-03-27 23:15:18 -0400

    Add pytest for create occasion method

--- server/tests/unit/test_sample.py
+++ server/tests/unit/test_sample.py
@@ -13,14 +13,26 @@ import base64
 # Test the /api/test endpoint
 def test_test_endpoint():
     with app.test_client() as client:
-        response = client.post('/api/test/Ellaria Sand')
+        response = client.get('/api/test/Ellaria Sand')
         assert response.status_code == 200
         assert response.data.decode('utf-8') == 'Voluptatum harum nobis recusandae exercitationem modi deserunt voluptates. Deleniti velit perferendis vitae quas excepturi quos esse deserunt.'
+
+# Test Calendar methods
+# Test the /api/createOccasion endpoint
+def test_create_occasion_endpoint():
+    with app.test_client() as client:
+        response = client.post('/api/createOccasion', json={
+            'userid': '61ed3b23749b5426fbbdd6ad',
+            'date': '2023-03-15',
+            'occasion': {'name': 'Test Occasion', 'date': '2023-03-15', 'planned_outfits': [], 'place': 'Home'}
+        })
+        assert response.status_code == 200
+        assert response.data.decode('utf-8') == '{"occasion_name":"Test Occasion","status":"success"}\n'
 
 # Test the /api/getOccationByDate endpoint
 def test_get_occation_by_date_endpoint():
     with app.test_client() as client:
-        response = client.get('/api/getOccationByDate', json={
+        response = client.post('/api/getOccationByDate', json={
             'userid': '61ed3b23749b5426fbbdd6ad',
             'date': '2023-03-15'
         })
@@ -34,6 +46,6 @@ def test_scan_new_item_endpoint():
         my_string = base64.b64encode(img_file.read())
         # my_string = str(my_string)
     with app.test_client() as client:
-        response = client.get('/api/ScanNewItem', data=my_string)
+        response = client.post('/api/ScanNewItem', data=my_string)
         assert response.status_code == 200
         assert response.data.decode('utf-8') == '{"item":"Shirt","status":"success"}\n'
