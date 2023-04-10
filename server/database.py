a57257520f41bc61a14955e04f587043ea845890
ZhouEEEEEE <zhoueric0603@gmail.com>
2023-04-10 16:45:56 -0700

    Solve certificate error

--- server/database.py
+++ server/database.py
@@ -1,7 +1,8 @@
 import pymongo
 import os
 from dotenv import load_dotenv
+import certifi
 
 load_dotenv()
-client = pymongo.MongoClient(os.environ.get("MONGODB_URL"))
+client = pymongo.MongoClient(os.environ.get("MONGODB_URL"),  tlsCAFile=certifi.where())
 
