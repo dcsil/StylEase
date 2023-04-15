d063772cc3a124e180e814c598f64df340dd11f6
GareArc <garethcxy.chen@mail.utoronto.ca>
2023-04-15 19:13:52 -0400

    fix bug in test

--- client/views/CalendarRoute/index.jsx
+++ client/views/CalendarRoute/index.jsx
@@ -67,25 +67,25 @@ export const CalendarRoute = ({ navigation }) => {
     setItemloaded(true);
   }
 
-    const renderEmptyDate = () => {
-      return (
-        <View style={styles.emptyDate}>
-          <Text variant="bodyLarge">This is empty date!</Text>
-        </View>
-      );
-    }
+  const renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text variant="bodyLarge">This is empty date!</Text>
+      </View>
+    );
+  }
 
-    useEffect(() => {
-      const unsubscribe = navigation.addListener(
-        'focus',
-        () => {
-            fetchCalendarDays(userId);
-            renderPage();
-        }
-      );
-      renderPage();
-      return unsubscribe;
-   }, [navigation.isFocused(), items, coverUris, itemloaded])
+  useEffect(() => {
+    const unsubscribe = navigation.addListener(
+      'focus',
+      () => {
+        fetchCalendarDays(userId);
+        renderPage();
+      }
+    );
+    renderPage();
+    return unsubscribe;
+  }, [navigation.isFocused(), items, coverUris, itemloaded])
 
   const renderItem = (item) => {
     return (
@@ -122,57 +122,58 @@ export const CalendarRoute = ({ navigation }) => {
         <Appbar.Header statusBarHeight={20} style={{ paddingBottom: 0 }}>
           <Appbar.Content title="Calendar" />
         </Appbar.Header>
-        <View style={{height: '90%'}}>
-        <Agenda
-          items={items}
-          loadItemsForMonth={month => {
-            return;
-          }}
-          selected={new Date().toJSON().slice(0, 10)}
-          onDayPress={(day) => {
-            setSelectedDate(day);
-          }}
-          onDayChange={(day) => {
-            setSelectedDate(day);
-          }}
-          showOnlySelectedDayItems={true}
-          pastScrollRange={50}
-          futureScrollRange={50}
-          renderItem={(item) => {
-            return renderItem(item);
-          }}
-          renderKnob={() => {
-            return renderKnob();
-          }}
-          rowHasChanged={(r1, r2) => {
-            return r1.text !== r2.text;
-          }}
-          renderEmptyData={()=>{
-            return renderEmptyDate();
-          }}
-          hideKnob={false}
-          showClosingKnob={true}
-          theme={{
-            todayTextColor: '#6a5acd',
-            dayTextColor: '#2d4150',
-            selectedDayBackgroundColor: '#6a5acd',
-            agendaTodayColor: '#6a5acd',
-            dotColor: '#6a5acd',
-          }}
-        />
+        <View style={{ height: '90%' }}>
+          <Agenda
+            items={items}
+            loadItemsForMonth={month => {
+              return;
+            }}
+            selected={new Date().toJSON().slice(0, 10)}
+            onDayPress={(day) => {
+              setSelectedDate(day);
+            }}
+            onDayChange={(day) => {
+              setSelectedDate(day);
+            }}
+            showOnlySelectedDayItems={true}
+            pastScrollRange={50}
+            futureScrollRange={50}
+            renderItem={(item) => {
+              return renderItem(item);
+            }}
+            renderKnob={() => {
+              return renderKnob();
+            }}
+            rowHasChanged={(r1, r2) => {
+              return r1.text !== r2.text;
+            }}
+            renderEmptyData={() => {
+              return renderEmptyDate();
+            }}
+            hideKnob={false}
+            showClosingKnob={true}
+            theme={{
+              todayTextColor: '#6a5acd',
+              dayTextColor: '#2d4150',
+              selectedDayBackgroundColor: '#6a5acd',
+              agendaTodayColor: '#6a5acd',
+              dotColor: '#6a5acd',
+            }}
+          />
         </View>
-      <FAB
-        style={styles.fab}
-        icon={(props) => <Icon name="plus" {...props} />}
-        onPress={() => navigation.navigate('Calendar-add-item', {
-          selectedDate: selectedDate,
-          userId: userId,
-        })}
-      />
-    </View>);
-    }
-    
-    return renderPage();
+        <FAB
+          testID='calender-add-item'
+          style={styles.fab}
+          icon={(props) => <Icon name="plus" {...props} />}
+          onPress={() => navigation.navigate('Calendar-add-item', {
+            selectedDate: selectedDate,
+            userId: userId,
+          })}
+        />
+      </View>);
+  }
+
+  return renderPage();
 };
 
 const styles = StyleSheet.create({
